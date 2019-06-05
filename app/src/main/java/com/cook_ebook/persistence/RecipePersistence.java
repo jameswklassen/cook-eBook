f49e32f225aa38890992e6ae6afd14d5c93000d6
Ruoshi Zhao <zhaor345@myumanitoba.ca>
2019-06-05 12:38:00 -0500

    Add getRecipeList-methods to RecipeRersistence: 1. getRecipeList(): get recipe list by date in ascending order 2. getRecipeListByXXX(): get recipe list by XXX

--- app/src/main/java/com/cook_ebook/persistence/RecipePersistence.java
+++ app/src/main/java/com/cook_ebook/persistence/RecipePersistence.java
@@ -2,7 +2,23 @@ package com.cook_ebook.persistence;
 
 import com.cook_ebook.objects.Recipe;
 
+import java.util.*;
+
 public interface RecipePersistence {
+
+    List<Recipe> getRecipeList(); //default sorted by date in ascending order
+
+    List<Recipe> getRecipeListByDescendingDate();
+
+    Recipe getRecipeById(int recipeId);
+
+    List<Recipe> getRecipeListByCookingTime(int cookingTime);
+
+    List<Recipe> getRecipeListByTag(String tag);
+
+    List<Recipe> getRecipeListByFavourite(boolean isFavourite);
+
+    List<Recipe> getRecipeListByDate(Date date);
 
     Recipe insertRecipe(Recipe recipe);
 
