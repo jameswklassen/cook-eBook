225ce1acb50cfd62155076b58bf4e7b4fda5e281
Raquel <thiess83@myumanitoba.ca>
2019-06-07 10:57:59 -0500

    Template for Add or Edit View
    This adds the view containing the text boxes for user to type in the specifications for their recipe.

---
+++ app/src/main/java/com/cook_ebook/presentation/AddEditView.java
@@ -0,0 +1,26 @@
+package com.cook_ebook.presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.cook_ebook.R;
+
+public class AddEditView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_add_edit_view);
+
+        //Add a back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_edit_view, menu);
+        return true;
+    }
+}
