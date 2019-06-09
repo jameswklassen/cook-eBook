429b6fc23cd6fc678cebb5b6c4455ac7c0a7a18d
Raquel <thiess83@myumanitoba.ca>
2019-06-09 10:50:21 -0500

    Connect Presentation Layer with Logic Layer

--- app/src/main/java/com/cook_ebook/presentation/MainActivity.java
+++ app/src/main/java/com/cook_ebook/presentation/MainActivity.java
@@ -13,6 +13,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
 
+import com.cook_ebook.objects.Recipe;
+import com.cook_ebook.logic.RecipeHandler;
 import com.cook_ebook.R;
 
 import java.util.ArrayList;
@@ -22,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     //Temporary variables until we have database placeholders merged in
-    private List<String> rNames = new ArrayList<>();
+    private List<Recipe> recipes = new ArrayList<>();
+    private RecipeHandler handler = new RecipeHandler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        addTestNames();
+        getRecipes();
     }
 
     @Override
@@ -54,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
         Intent myIntent = new Intent(getBaseContext(), AddEditView.class);
         startActivity(myIntent);
     }
-    
+
     //TODO Add delete function
     public void deleteRecipe(){
         // delete recipe
@@ -80,26 +83,22 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void addTestNames() {
-        rNames.add("Chicken");
-        rNames.add("Spinach");
-        rNames.add("Omelet");
-        rNames.add("Cookie");
-
-        for(int i = 0; i < 100; i++)
+    private void getRecipes() {
+        List<String> names = new ArrayList<>();
+        recipes = handler.getAllRecipes();
+        for(int i = 0; i < recipes.size(); i++)
         {
-            rNames.add("Test Item #" + (i+1));
+            names.add(recipes.get(i).getRecipeTitle());
         }
 
-
-        initRecyclerView();
+        initRecyclerView(recipes);
     }
 
-    private void initRecyclerView() {
+    private void initRecyclerView(List<Recipe> recipes) {
         Log.d(TAG, "InitRecyclerView: init recycler view.");
 
         RecyclerView recyclerView = findViewById(R.id.recycleView);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(rNames, this);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(recipes, this);
 
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
