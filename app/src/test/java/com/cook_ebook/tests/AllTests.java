62f38ecc38662b949c66b152877103c5eab9d257
Raquel Thiessen <thiess83@myumanitoba.ca>
2019-06-21 09:23:15 -0500

    Update AllTests File to include all tests

--- app/src/test/java/com/cook_ebook/tests/AllTests.java
+++ app/src/test/java/com/cook_ebook/tests/AllTests.java
@@ -4,6 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 // Import the individual test files
+import com.cook_ebook.logic.RecipeHandler;
+import com.cook_ebook.objects.RecipeTagSet;
 import com.cook_ebook.tests.business.InitBusinessTest;
 import com.cook_ebook.tests.objects.InitObjectTest;
 
@@ -13,7 +15,9 @@ import com.cook_ebook.tests.objects.RecipeTest;
 @Suite.SuiteClasses({
         InitBusinessTest.class,
         InitObjectTest.class,
-        RecipeTest.class
+        RecipeTest.class,
+        RecipeHandler.class,
+        RecipeTagSet.class
 })
 
 public class AllTests
