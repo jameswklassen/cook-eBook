31eb99f7e3afe1597b38604d39087551fc46edb2
Robyn Zhao <zhaorobyn895@gmail.com>
2019-07-12 19:22:36 -0500

    edit RecipeHandleIT

--- app/src/test/java/com/cook_ebook/tests/IntegrationTests.java
+++ app/src/test/java/com/cook_ebook/tests/IntegrationTests.java
@@ -1,12 +1,15 @@
 package com.cook_ebook.tests;
 
+import com.cook_ebook.tests.business.RecipeHandlerIT;
+import com.cook_ebook.tests.business.RecipeTagHandlerIT;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-
+        RecipeHandlerIT.class
 })
 
 public class IntegrationTests {
--- app/src/test/java/com/cook_ebook/tests/business/RecipeHandlerIT.java
+++ app/src/test/java/com/cook_ebook/tests/business/RecipeHandlerIT.java
@@ -1,4 +1,40 @@
 package com.cook_ebook.tests.business;
 
+import com.cook_ebook.logic.RecipeHandler;
+import com.cook_ebook.tests.utils.TestUtils;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertNotNull;
+
 public class RecipeHandlerIT {
+    private RecipeHandler recipeHandler;
+    private File tempDB;
+
+    @Before
+    public void setUp() throws IOException {
+        System.out.println("Starting integration test for RecipeHandler");
+        this.tempDB = TestUtils.copyDB();
+        this.recipeHandler = new RecipeHandler(true);
+
+        assertNotNull(this.recipeHandler);
+    }
+
+    @Test
+    public void testRecipeList() {
+        System.out.println("\nStarting testCreateRecipeList");
+
+        System.out.println("Finished testCreateRecipeList");
+    }
+
+    @After
+    public void tearDown() {
+        // reset DB
+        this.tempDB.delete();
+    }
 }
