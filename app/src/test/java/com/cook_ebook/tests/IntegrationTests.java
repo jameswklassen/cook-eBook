350b7ebf8a89ff946f5e1d500f2a589c41fdf192
Robyn Zhao <zhaorobyn895@gmail.com>
2019-07-14 12:26:52 -0500

    create integration test for RecipeTagHandler, method testGetAllTags()

--- app/src/test/java/com/cook_ebook/tests/IntegrationTests.java
+++ app/src/test/java/com/cook_ebook/tests/IntegrationTests.java
@@ -9,7 +9,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        RecipeHandlerIT.class
+        RecipeHandlerIT.class,
+        RecipeTagHandlerIT.class
 })
 
 public class IntegrationTests {
